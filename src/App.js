b5bfd9373eab8d55b51c7f2b1085c7f145c0f69b
HiteshhK <kaushikhitesh002@gmail.com>
2022-01-06 22:33:29 +0530

    6 files modified final changes for responsiveness

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+// import coronaIMage from './images/image.png';
 
 class App extends React.Component {
   state = {
     data: {},
     country: '',
   };
+  coronaIMage = `https://i.ibb.co/7QpKsCX/image.png`;
 
   async componentDidMount() {
     const fetchedData = await fetchData();
@@ -18,11 +20,11 @@ class App extends React.Component {
     const fetchedData = await fetchData(country);
     this.setState({ data: fetchedData, country });
   };
-
   render() {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img src={this.coronaIMage} className={styles.image} alt="COVID-19" />
         <Cards data={this.state.data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
